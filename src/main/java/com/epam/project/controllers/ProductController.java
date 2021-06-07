1d165a1f891a43a59e9d320a4f06aa7427fd3cde
Konstantin_Shnyrkov <konstantin.shnyrkov@epam.com>
2021-06-07 14:00:45 +0300

    initial commit

---
+++ src/main/java/com/epam/project/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.epam.project.controllers;
+
+import com.epam.project.data.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/product/2")
+@Slf4j
+public class ProductController {
+
+    @PutMapping("/add")
+    public void add(@RequestBody Product product) {
+        log.info("Add method was invoke {}", product);
+    }
+
+    @GetMapping("/get")
+    public @ResponseBody
+    Product get() {
+        log.info("Get method was invoke");
+        return Product.builder().id(UUID.randomUUID()).name("new-product").build();
+    }
+}
